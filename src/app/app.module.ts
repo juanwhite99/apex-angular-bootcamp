a3b4dc95349dd4ecec09c97aae00916dafb3b48b
Juan Pablo L <juan.pablo.lb99@gmail.com>
2024-03-19 19:17:28 -0600

    1 - Directivas de atributo
    chore: add first module and component
    
    1 - Directivas de atributo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { AttributeDirectivesComponent } from './attribute-directives/attribute-directives.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AttributeDirectivesComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule
   ],
   providers: [],
