b6be893e9eb100adb25e5f93274bb568e1e0c0e3
Juan Pablo L <juan.pablo.lb99@gmail.com>
2024-04-05 20:14:22 -0600

    7-pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PipeExerciseComponent } from './pipe-exercise/pipe-exercise.component';
+import { CapitalizePipe } from './pipes/capitalize.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PipeExerciseComponent,
+    CapitalizePipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pipe-exercise/pipe-exercise.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipe-exercise',
+  templateUrl: './pipe-exercise.component.html'
+})
+export class PipeExerciseComponent {
+  public numVar: number = 34.22;
+  public strVar: string = 'hello world';
+
+}
